e67c5aedf624c1f143babfac4bdd0a4d27901d88
sidneyxvr <sidneyxvr@gmail.com>
2018-08-14 22:06:44 -0300

    sucesso

--- sniffing.cpp
+++ sniffing.cpp
@@ -1,13 +1,13 @@
 #include <bits/stdc++.h>
 #include <tins/tins.h>
-
+//g++ sniffing.cpp -ltins
 using namespace Tins;
 using namespace std;
 
 map<string, int> ip_orig, ip_dest;
-int _count, sum;
+int _count, soma, i;
 
-string ip_m(map<string, int> m)
+string ip_mais_acessado(map<string, int> m)
 {
     pair<string, int> p = {"", -1};
     for(auto a: m)
@@ -18,7 +18,7 @@ string ip_m(map<string, int> m)
     return p.first;
 }
 
-string cvt(auto t)
+string to_string(auto t)
 {
     stringstream ss;
     ss << t;
@@ -27,20 +27,20 @@ string cvt(auto t)
     return str;
 }
 
-void estatisticas(string src, string dst, string s_port, string d_port, int len)
+void pacote_info(string src, string dst, string s_port, string d_port, int tamanho)
 {
     ip_orig[src]++;
     ip_dest[dst]++;
-    sum += len;
+    soma += tamanho;
     _count++;
     cout << "====================TCP=========================\n";
     cout << "IP de origem                  : " << src << endl;
     cout << "IP de destino                 : " << dst << endl;
-    cout << "Tamanho do pacote             : " << len << endl;
+    cout << "Tamanho do pacote             : " << tamanho << endl;
     cout << "================================================\n\n";
 }
-int i;
-bool callback(const PDU &pdu) 
+
+bool rastrear(const PDU &pdu) 
 {
     if(i == 1000) return false;
     i++;
@@ -48,22 +48,22 @@ bool callback(const PDU &pdu)
     if(ip.protocol() == 6)
     {
         const TCP &tcp = pdu.rfind_pdu<TCP>(); 
-        estatisticas(cvt(ip.src_addr()), cvt(ip.dst_addr()), cvt(tcp.sport()), cvt(tcp.dport()), tcp.size());
+        pacote_info(to_string(ip.src_addr()), to_string(ip.dst_addr()), to_string(tcp.sport()), to_string(tcp.dport()), tcp.size());
     }
     else if(ip.protocol() == 17)
     {
         const UDP &udp = pdu.rfind_pdu<UDP>(); 
-        estatisticas(cvt(ip.src_addr()), cvt(ip.dst_addr()), cvt(udp.sport()), cvt(udp.dport()), udp.size());
+        pacote_info(to_string(ip.src_addr()), to_string(ip.dst_addr()), to_string(udp.sport()), to_string(udp.dport()), udp.size());
     }
     return true;
 }
 
 int main() {
-    Sniffer("enp1s0").sniff_loop(callback);
+    Sniffer("enp1s0").sniff_loop(rastrear);
     cout << "================== Resultado ===================\n";
-    cout << "Total pacotes                 : " << sum << endl;
-    cout << "Media                         : " << sum / _count << endl;
-    cout << "IP destino mais acessado      : " << ip_m(ip_dest) << endl;
-    cout << "IP origem que mais transmitiu : " << ip_m(ip_orig) << endl;
+    cout << "Tamanho total dos pacotes     : " << soma << " B" << endl;
+    cout << "Media                         : " << (soma / _count) << " B" << endl;
+    cout << "IP destino mais acessado      : " << ip_mais_acessado(ip_dest) << endl;
+    cout << "IP origem que mais transmitiu : " << ip_mais_acessado(ip_orig) << endl;
     cout << "================== ========= ===================\n";
 }
